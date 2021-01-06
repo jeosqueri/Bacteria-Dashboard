afd8b42aa85553ef100d6dd02219c2124a23dc7d
jeosqueri <juliasqueri303@gmail.com>
2021-01-05 18:41:56 -0700

    got arrays working, dropdown menu working, ids on dropdown menu display correct arrays in console

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -5,11 +5,11 @@ d3.json(dataset).then(function(data) {
     console.log(data);    //work with data inside this
   });
 
-function unpack(rows, index) {
-    return rows.map(function(row) {
-    return row[index];
-    });
-    }
+// function unpack(rows, index) {
+//     return rows.map(function(row) {
+//     return row[index];
+//     });
+//     }
 // Call updatePlotly() when a change takes place to the DOM
 d3.selectAll("#selDataset").on("change", updatePlot);
 
@@ -26,26 +26,47 @@ function updatePlot() {
     buildPlot(dropdownMenu);
   }
 
+var dropdownValues = d3.json(dataset).then((data) => {
+  var id = data.names;
+  console.log(id);
+  var dropdownSelect = d3.select('#selDataset');
+  Object.entries(id).forEach(([key, value]) => {
+    dropdownSelect.append('option').text(value);
+  });
+})
+
+console.log(dropdownValues);
+
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-function buildPlot(dropdownMenu) {
+function buildPlot(subID) {
     var dataset = "samples.json";
 
     d3.json(dataset).then(function(data) {
     console.log(data);    //work with data inside this
     var samples = data.samples;
     console.log(samples);
+    var subjectID = samples.map(row => row.id).indexOf(subID);
+    console.log(subjectID);
     
-    var sampleValues = data.samples.map(row => row[2]);
-    var otuIds = data.samples.map(row => row[1]);
-    var otuLabels = data.samples.map(row => row[3]);
+    var sampleValues = samples.map(row => row.sample_values);
+    var sampleVals = sampleValues[subjectID].slice(0,10).reverse();
+    var otuIds = data.samples.map(row => row.otu_ids);
+    var otuIdsSub = otuIds[subjectID].slice(0,10);
+    var otuLabels = data.samples.map(row => row.otu_labels);
+    var otuLabelsSub = otuLabels[subjectID].slice(0,10);
     console.log(sampleValues);
+    console.log(sampleVals);
+    console.log(otuIds);
+    console.log(otuIdsSub);
+    console.log(otuLabels);
+    console.log(otuLabelsSub);
 
     var trace1 = {
         type: 'bar',
-        x: sampleValues,
-        y: otuIds,
+        x: sampleVals,
+        y: otuIdsSub,
         orientation: 'h',
-        text: otuLabels
+        text: otuLabelsSub
     }
 
     var data = [trace1];
@@ -62,8 +83,10 @@ function buildPlot(dropdownMenu) {
         }
       };
 
-    Plotly.newPlot("plot", data, layout);
+    Plotly.newPlot('#selDataset', data, layout);
     });
 }
+// Call updatePlotly() when a change takes place to the DOM
+d3.selectAll("#selDataset").on("change", updatePlot);
 
-buildPlot();
+//updatePlot();
